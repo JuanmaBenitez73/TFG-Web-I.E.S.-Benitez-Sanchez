3257c6b0a8f625f2eaedef45c0b0c15617d5d068
JuanmaBenitez73 <benitezjuanma25@gmail.com>
2025-05-19 14:26:57 +0000

    Big Commit

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,147 @@
 
 namespace App\Controller;
 
+use App\Entity\Home;
+use App\Form\HomeType;
+use App\Repository\HomeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\Form\FormError;
 
+#[Route('/home')]
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
+    #[Route('/', name: 'app_home_index', methods: ['GET'])]
+    public function index(HomeRepository $homeRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('home/index.html.twig', [
+            'homes' => $homeRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_home_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
+    {
+        $home = new Home();
+        $form = $this->createForm(HomeType::class, $home);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $imageFile = $form->get('image')->getData();
+            if ($imageFile) {
+                $allowedExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+                $extension = strtolower($imageFile->getClientOriginalExtension());
+
+                if (!in_array($extension, $allowedExtensions)) {
+                    $this->addFlash('error', 'Solo se permiten imágenes JPG, PNG o GIF');
+                } else {
+                    $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                    $safeFilename = $slugger->slug($originalFilename);
+                    $newFilename = $safeFilename . '-' . uniqid() . '.' . $extension;
+                    try {
+                        $imageFile->move(
+                            $this->getParameter('images_directory_home'),
+                            $newFilename
+                        );
+                    } catch (FileException $e) {
+                        $this->addFlash('error', 'No se pudo subir la imagen: ' . $e->getMessage());
+                    }
+                    $home->setImage($newFilename);
+                }
+            }
+
+            $entityManager->persist($home);
+            $entityManager->flush();
+
+            return $this->redirect('/', Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('home/new.html.twig', [
+            'home' => $home,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_home_show', methods: ['GET'])]
+    public function show(Home $home): Response
+    {
+        return $this->render('home/show.html.twig', [
+            'home' => $home,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_home_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Home $home, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
+    {
+        $form = $this->createForm(HomeType::class, $home);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $imageFile = $form->get('image')->getData();
+
+            if ($imageFile) {
+                $allowedExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+                $extension = strtolower($imageFile->getClientOriginalExtension());
+
+                if (!in_array($extension, $allowedExtensions)) {
+                    $this->addFlash('error', 'Solo se permiten imágenes JPG, PNG o GIF');
+                } else {
+                    $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                    $safeFilename = $slugger->slug($originalFilename);
+                    $extension = pathinfo($imageFile->getClientOriginalName(), PATHINFO_EXTENSION);
+                    $newFilename = $safeFilename . '-' . uniqid() . '.' . $extension;
+
+                    $imageFile->move(
+                        $this->getParameter('images_directory_home'),
+                        $newFilename
+                    );
+
+                    $oldFilename = $home->getImage();
+                    if ($oldFilename) {
+                        $oldImagePath = $this->getParameter('images_directory_home') . '/' . $oldFilename;
+                        if (file_exists($oldImagePath)) {
+                            unlink($oldImagePath);
+                        }
+                    }
+
+                    $home->setImage($newFilename);
+                }
+            }
+
+            $entityManager->flush();
+
+            return $this->redirect('/', Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('home/edit.html.twig', [
+            'home' => $home,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_home_delete', methods: ['POST'])]
+    public function delete(Request $request, Home $home, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $home->getId(), $request->getPayload()->get('_token'))) {
+            $imageFilename = $home->getImage();
+
+            if ($imageFilename) {
+                $imagePath = $this->getParameter('images_directory_home') . '/' . $imageFilename;
+
+                if (file_exists($imagePath)) {
+                    unlink($imagePath);
+                }
+            }
+
+            $entityManager->remove($home);
+            $entityManager->flush();
+        }
+
+        return $this->redirect('/', Response::HTTP_SEE_OTHER);
     }
 }
---
+++ src/Form/HomeType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Home;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
+class HomeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('image', FileType::class, [
+                'label' => 'Imagen',
+                'mapped' => false, // Muy importante: evita que Symfony intente asignar el archivo directamente al string de la entidad
+                'required' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Home::class,
+        ]);
+    }
+}
